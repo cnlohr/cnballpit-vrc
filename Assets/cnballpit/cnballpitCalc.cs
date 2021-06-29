f7f75bf7a735e57ba09b1ba8da9e7a793fd37640
cnlohr <lohr85@gmail.com>
2021-06-29 02:15:00 -0700

    Cleanup

--- Assets/cnballpit/cnballpitCalc.cs
+++ Assets/cnballpit/cnballpitCalc.cs
@@ -41,32 +41,26 @@ public class cnballpitCalc : UdonSharpBehaviour
 
     void Start()
     {
-		//Comparison: 
-		//	Totally Disabled:  9.4-11.1
+		//Tricky:  Call SetTargetBuffers in the order you want the cameras to execute.
+		RenderBuffer[] CAR0A = new RenderBuffer[] { CAR0.colorBuffer };
+		CamAdj0.SetTargetBuffers( CAR0A, CAR0.depthBuffer );
+		RenderBuffer[] CAR1A = new RenderBuffer[] { CAR1.colorBuffer };
+		CamAdj1.SetTargetBuffers( CAR1A, CAR1.depthBuffer );
+		RenderBuffer[] CAR2A = new RenderBuffer[] { CAR2.colorBuffer };
+		CamAdj2.SetTargetBuffers( CAR2A, CAR2.depthBuffer );
+		RenderBuffer[] CAR3A = new RenderBuffer[] { CAR3.colorBuffer };
+		CamAdj3.SetTargetBuffers( CAR3A, CAR3.depthBuffer );
 		
-		if ( true )
-		{
-			RenderBuffer[] CAR0A = new RenderBuffer[] { CAR0.colorBuffer };
-			CamAdj0.SetTargetBuffers( CAR0A, CAR0.depthBuffer );
-			RenderBuffer[] CAR1A = new RenderBuffer[] { CAR1.colorBuffer };
-			CamAdj1.SetTargetBuffers( CAR1A, CAR1.depthBuffer );
-			RenderBuffer[] CAR2A = new RenderBuffer[] { CAR2.colorBuffer };
-			CamAdj2.SetTargetBuffers( CAR2A, CAR2.depthBuffer );
-			RenderBuffer[] CAR3A = new RenderBuffer[] { CAR3.colorBuffer };
-			CamAdj3.SetTargetBuffers( CAR3A, CAR3.depthBuffer );
-			
-			RenderBuffer[] renderBuffersB = new RenderBuffer[] { rtPositionB.colorBuffer, rtVelocityB.colorBuffer };
-			CamCalcB.SetTargetBuffers(renderBuffersB, rtPositionB.depthBuffer);
-			
-			CamAdj4.SetTargetBuffers( CAR0A, CAR0.depthBuffer );
-			CamAdj5.SetTargetBuffers( CAR1A, CAR1.depthBuffer );
-			CamAdj6.SetTargetBuffers( CAR2A, CAR2.depthBuffer );
-			CamAdj7.SetTargetBuffers( CAR3A, CAR3.depthBuffer );
+		RenderBuffer[] renderBuffersB = new RenderBuffer[] { rtPositionB.colorBuffer, rtVelocityB.colorBuffer };
+		CamCalcB.SetTargetBuffers(renderBuffersB, rtPositionB.depthBuffer);
+		
+		CamAdj4.SetTargetBuffers( CAR0A, CAR0.depthBuffer );
+		CamAdj5.SetTargetBuffers( CAR1A, CAR1.depthBuffer );
+		CamAdj6.SetTargetBuffers( CAR2A, CAR2.depthBuffer );
+		CamAdj7.SetTargetBuffers( CAR3A, CAR3.depthBuffer );
 
-			RenderBuffer[] renderBuffersA = new RenderBuffer[] { rtPositionA.colorBuffer, rtVelocityA.colorBuffer };
-			CamCalcA.SetTargetBuffers(renderBuffersA, rtPositionA.depthBuffer);
-
-		}
+		RenderBuffer[] renderBuffersA = new RenderBuffer[] { rtPositionA.colorBuffer, rtVelocityA.colorBuffer };
+		CamCalcA.SetTargetBuffers(renderBuffersA, rtPositionA.depthBuffer);
 
 		AccumulatedFrameBoundary = 0;
     }
