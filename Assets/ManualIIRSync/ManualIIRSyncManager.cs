61693f6e1010033cd4c79d550bf99a3fef2bb915
cnlohr <lohr85@gmail.com>
2021-07-22 06:29:01 -0400

    Update with manual IIR Sync.

---
+++ Assets/ManualIIRSync/ManualIIRSyncManager.cs
@@ -0,0 +1,70 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ManualIIRSyncManager : UdonSharpBehaviour
+{
+	ManualIIRSyncObject [] UpdateObjectList;
+	int UpdateObjectListCount = 0;
+	
+	
+    void Start()
+    {
+   		if( UpdateObjectList == null )
+		{
+			UpdateObjectList = new ManualIIRSyncObject[1000];
+		}
+
+    }
+	
+	public void RegisterSubscriptionB( ManualIIRSyncObject go )
+	{
+		if( UpdateObjectList == null )
+		{
+			UpdateObjectList = new ManualIIRSyncObject[1000];
+		}
+		if( UpdateObjectListCount < 1000 )
+		{
+			UpdateObjectList[UpdateObjectListCount] = go;
+			UpdateObjectListCount++;
+		}
+	}
+	
+	public void UnregisterSubscriptionB( ManualIIRSyncObject go )
+	{
+		if( UpdateObjectList == null )
+		{
+			UpdateObjectList = new ManualIIRSyncObject[1000];
+		}
+
+		int i;
+		for( i = 0; i < UpdateObjectListCount; i++ )
+		{
+			if( UpdateObjectList[i] == go )
+			{
+				UpdateObjectListCount--;
+				//Remove from list and update list.
+				for( ; i < UpdateObjectListCount; i++ )
+				{
+					UpdateObjectList[i] = UpdateObjectList[i+1];
+				}
+			}
+		}
+	}
+	
+	public void Update()
+	{
+		int i;
+		Debug.Log( UpdateObjectListCount );
+		for( i = 0; i < UpdateObjectListCount; i++ )
+		{
+			ManualIIRSyncObject behavior = UpdateObjectList[i];
+			if( behavior != null )
+			{
+				behavior.OnSubscriptionUpdate();
+			}
+		}
+	}
+}
---
+++ Assets/ManualIIRSync/ManualIIRSyncObject.cs
@@ -0,0 +1,103 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ManualIIRSyncObject : UdonSharpBehaviour
+{
+	[UdonSynced] public Vector3 SyncPosition;
+	[UdonSynced] public bool    SyncMoving;
+	[UdonSynced] public Quaternion SyncRotation;
+	
+	private bool WasMoving;
+	private Collider thisCollider;
+	private ManualIIRSyncManager dispatchManager;
+	private bool MasterMoving;
+	
+    void Start()
+    {
+		dispatchManager = GameObject.Find( "SmartUpdateManager" ).GetComponent<ManualIIRSyncManager>();
+        thisCollider = GetComponent<Collider>();
+		WasMoving = false;
+		MasterMoving = false;
+    }
+
+	public void SendMasterMove()
+	{
+		SyncPosition = transform.localPosition;
+		SyncRotation = transform.localRotation;
+		
+		//We are being moved.
+		RequestSerialization();
+	}
+
+    override public void OnPickup ()
+    {
+		thisCollider.enabled = false;
+		dispatchManager.RegisterSubscriptionB( this );
+		Networking.SetOwner( Networking.LocalPlayer, gameObject );
+		SyncMoving = true;
+		MasterMoving = true;
+    }
+
+    override public void OnDrop()
+    {
+		dispatchManager.UnregisterSubscriptionB( this );
+		SyncMoving = false;
+		thisCollider.enabled = true;
+		MasterMoving = false;
+		SendMasterMove();
+    }
+	
+	public override void OnDeserialization()
+	{
+		if( !MasterMoving )
+		{
+			if( WasMoving )
+			{
+				if( !SyncMoving )
+				{
+					//We were released.
+					transform.localPosition = SyncPosition;
+					transform.localRotation = SyncRotation;
+					WasMoving = false;
+					dispatchManager.UnregisterSubscriptionB( this );
+				}
+			}
+			else
+			{
+				if( SyncMoving )
+				{
+					dispatchManager.RegisterSubscriptionB( this );
+				}
+			}
+		}
+		else
+		{
+			if( Networking.GetOwner( gameObject ) != Networking.LocalPlayer )
+			{
+				//Master is moving AND another player has it.
+				((VRC_Pickup)(gameObject.GetComponent(typeof(VRC_Pickup)))).Drop();
+			}
+		}
+	}
+	
+	public void OnSubscriptionUpdate()
+	{
+		if( MasterMoving )
+		{
+			SendMasterMove();
+		}
+		else
+		{
+			//Moving Started
+			Vector3 lpr = transform.localPosition;
+			//Still moving, make motion slacky.
+			transform.localPosition = lpr * .7f + SyncPosition * .3f;
+			transform.localRotation = Quaternion.Slerp( transform.localRotation, SyncRotation, .3f ); 
+			
+			WasMoving = true;
+		}
+	}
+}
